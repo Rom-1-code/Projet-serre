6ae39f9c7db22b7371b19597c15c9058c9d9940a
FLEMAL <55393709+Rom-1-code@users.noreply.github.com>
2021-05-03 22:09:15 +0200

    commit bdd

---
+++ C++/BDD/BDD/Main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "mysql.h"
+#include <cstring>
+
+using namespace std;
+
+int main() {
+	//Déclaration de la structure de type MYSQL
+	MYSQL mysql;
+	char response;
+	int result;
+	const char * request = "INSERT INTO `ValeurNum` (`ID_Valeur`, `Température_int`, `Température_Ext`, `HumiditéAir`, `HumiditéTerre`, `Date`, `Heure`, `Etat`) VALUES ('2', '30', '40', '40', '30', '2021-02-01', '07:00:00', '1');";
+	const char * host = "mysql-projetserre.alwaysdata.net";
+	const char * login = "231031_admin";
+	const char * password = "serre1234";
+	const char * bdd = "projetserre_sql";
+
+	
+	mysql_init(&mysql);
+	//Si la connexion réussie..
+	if (mysql_real_connect(&mysql, host, login, password, bdd, 0, NULL, 0))
+	{
+		
+		result = mysql_real_query(&mysql, request, strlen(request));
+
+		if (result == 0)
+		{
+			cout << "Insertion en base OK !" << endl;
+		}
+		else
+		{
+			cout << "Insertion en base PAS OK ! " << endl;
+		}
+	}
+	//Sinon ...
+	else
+	{
+		cout << "Une erreur s'est produite lors de la connexion à la BDD!" << endl;
+	}
+
+	mysql_close(&mysql);
+
+	return 0;
+}
+
